c6eceb34c9e60e88110fb57253775227706e58f2
Yurii Musolov <yurii.musolov@gmail.com>
2024-11-08 16:15:19 +0100

    feat(cli-word-counter): add word frequencies

--- cli-word-counter/src/main.rs
+++ cli-word-counter/src/main.rs
@@ -22,26 +22,42 @@ pub fn word_count(line: &str) -> u64 {
     word_counter.get_count()
 }
 
+pub fn word_frequencies<'a>(line: &'a str) -> Vec<(&'a str, u64)> {
+    let mut word_counter = WordCounter::new();
+    line.split(" ")
+        .into_iter()
+        .map(|word| word.trim() /* TODO: remove [,.!?:;] */)
+        .filter(|word| word.len() > 0)
+        .for_each(|word| word_counter.add_word(word));
+    word_counter.get_frequencies()
+}
+
 pub struct WordCounter<'a> {
-    words: HashMap<&'a str, u64>,
+    frequencies: HashMap<&'a str, u64>,
 }
 
 impl<'a> WordCounter<'a> {
     pub fn new() -> Self {
         Self {
-            words: HashMap::new(),
+            frequencies: HashMap::new(),
         }
     }
 
     pub fn clear(&mut self) {
-        self.words = HashMap::new();
+        self.frequencies = HashMap::new();
     }
 
     pub fn add_word(&mut self, word: &'a str) {
-        *self.words.entry(word).or_insert(0) += 1;
+        *self.frequencies.entry(word).or_insert(0) += 1;
     }
 
     pub fn get_count(&self) -> u64 {
-        self.words.values().into_iter().sum()
+        self.frequencies.values().into_iter().sum()
+    }
+
+    pub fn get_frequencies(&self) -> Vec<(&'a str, u64)> {
+        let mut list: Vec<_> = self.frequencies.clone().into_iter().collect();
+        list.sort_by(|(_, v1), (_, v2)| v2.partial_cmp(v1).unwrap());
+        list
     }
 }
